7ac3b11138e34c615d748a7bfa0a9f97de18f382
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-09 10:10:11 +0100

    fix: equals

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ const elementGrid = document.getElementById("grid");
 for (let number = 1; number <= 100; number++) {
   const box = document.createElement("li");
   box.classList.add("box");
-  if (number % 3 == 0 && number % 5 == 0) {
+  if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
     box.append("FizzBuzz");
     elementGrid.append(box);
     box.classList.add("fizzbuzz");
-  } else if (number % 3 == 0) {
+  } else if (number % 3 === 0) {
     console.log("Fizz");
     box.append("Fizz");
     elementGrid.append(box);
     box.classList.add("fizz");
-  } else if (number % 5 == 0) {
+  } else if (number % 5 === 0) {
     console.log("Buzz");
     box.append("Buzz");
     elementGrid.append(box);
